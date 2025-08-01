e31663c603d1c342ff6ffebaf5c2bcf7234d7029
ashish-dst <ashish.dstdsu@gmail.com>
2025-08-01 15:26:56 +0530

    added ifd

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ logo_right = get_base64_image("dst.png")
 color_map = {
     "Budget": "#FFF3E3",       # Pink
     "Expenditure": "#6CCCA9",   # Light green
-    "InProcess": "#ADD8E6"          # Light blue
+    "IFD": "#ADD8E6"          # Light blue
 
 }
 
